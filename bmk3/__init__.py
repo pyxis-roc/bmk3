0eafef0694696bb89cff72bd7a9026138d3d7480
Sreepathi Pai <sree@cs.rochester.edu>
2021-10-26 16:41:24 -0400

    Prototype templating support

--- bmk3/__init__.py
+++ bmk3/__init__.py
@@ -1,8 +1,129 @@
 from string import Formatter
+import string
+import yaml
+import re
+import sys
+import itertools
 
+ARG_NAME = re.compile(r"[^.\[]+")
 class ScriptTemplate:
+    def __init__(self, name, template):
+        self.name = name
+        self.template = template
+        self.parse()
+
+    def parse(self):
+        f = Formatter()
+
+        out = []
+        v = []
+        try:
+            for e in f.parse(self.template):
+                fieldname = e[1]
+                if fieldname is not None:
+                    arg_name = ARG_NAME.match(fieldname)
+                    assert arg_name is not None, fieldname
+                    arg_name = arg_name.group(0)
+
+                    if arg_name[0] in string.digits:
+                        raise ValueError(f"bmk3 templates do not support positional arguments: {arg_name}.")
+                    v.append(arg_name)
+
+                out.append(e)
+        except ValueError as err:
+            print(err, file=sys.stderr)
+            print(f"when parsing template '{self.name}':", file=sys.stderr)
+            print(self.template, file=sys.stderr)
+            sys.exit(1) # TODO
+
+        self.parsed = out
+        self._varrefs = v
+        self.variables = set(v)
+
+    def expand_templates(self, templates):
+        parsed = self.parsed
+        changed = True
+
+        while changed:
+            changed = False
+            out = []
+            for x in parsed:
+                fieldname = x[1]
+
+                if fieldname is not None:
+                    arg_name = ARG_NAME.match(fieldname)
+                    assert arg_name is not None, fieldname
+                    arg_name = arg_name.group(0)
+
+                    if arg_name == 'templates':
+                        tmpl = fieldname[len('templates['):][:-1]
+                        changed = True
+                        out.extend(templates[tmpl].parsed)
+                    else:
+                        out.append(x)
+                else:
+                    out.append(x)
+
+            parsed = out
+
+        template = []
+        for x in parsed:
+            template.append(x[0])
+
+            if x[1] is not None:
+                out = "{" + x[1]
+                if x[2]: out = out + "!" + x[2]
+                if x[3]: out = out + ":" + x[3]
+                out = out + "}"
+                template.append(out)
+
+        self.template = ''.join(template)
+        self.parse()
+
+    def generate(self, varvals):
+        vk = set(varvals.keys())
+        not_provided = self.variables - vk
+
+        if len(not_provided):
+            raise KeyError(f"Variables {not_provided} not specified for rule {self.name}")
+
+        varorder = []
+        varcontents = []
+        for v in self.variables:
+            varorder.append(v)
+            if isinstance(varvals[v], list):
+                # this means that lists must be doubly-nested [[]] to be treated as singletons
+                varcontents.append(varvals[v])
+            else:
+                varcontents.append([varvals[v]])
+
+        for x in itertools.product(*varcontents):
+            assign = dict([(v, xx) for v, xx in zip(varorder, x)])
+
+            s = self.template.format(**assign)
+
+            yield assign, s
+
+    def __str__(self):
+        return self.template #f"ScriptTemplate({self.name}, {repr(self.template)})"
+
+    __repr__ = __str__
+
+class Script:
     def __init__(self, script):
         self.script = script
 
+        with open(self.script, "r") as f:
+            self._contents = yaml.safe_load(f)
+            self._variables = self._contents['variables']
 
-    
+            templates = self._contents['templates']
+            self._templates = dict([(v, ScriptTemplate(v, templates[v])) for v in templates])
+
+    @property
+    def variables(self):
+        return self._variables
+
+    @property
+    def templates(self):
+        return self._templates
